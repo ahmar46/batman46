f02d6b88550dfac98ec4898145939d1c305c67f4
ahmar46 <ahmarfreelancer@gmail.com>
2025-07-23 18:20:38 +0530

    Code

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function toggleMenu() {
-      const nav = document.getElementById('navLinks');
-      nav.style.display = nav.style.display === 'flex' ? 'none' : 'flex';
-    }
+  const nav = document.getElementById("navLinks");
+  nav.style.display = nav.style.display === "flex" ? "none" : "flex";
+}
+
+function toggleMenu() {
+  const nav = document.getElementById("navLinks");
+  nav.classList.toggle("active");
+}
