3e16beea91a891b5036df9d0420be38dc11b63d8
ahmar46 <ahmarfreelancer@gmail.com>
2025-07-23 18:37:42 +0530

    basic prototype made

--- script.js
+++
@@ -1,9 +0,0 @@
-function toggleMenu() {
-  const nav = document.getElementById("navLinks");
-  nav.style.display = nav.style.display === "flex" ? "none" : "flex";
-}
-
-function toggleMenu() {
-  const nav = document.getElementById("navLinks");
-  nav.classList.toggle("active");
-}
